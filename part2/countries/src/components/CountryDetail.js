bb409f06efaab4e167288fbc11946474f7528738
leanrobert <leanrobert24@gmail.com>
2023-03-05 20:41:41 -0300

    Exercise 2.20

--- part2/countries/src/components/CountryDetail.js
+++ part2/countries/src/components/CountryDetail.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const CountryDetail = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&appid=${process.env.REACT_APP_APIKEY}`)
+        .then(res => {
+          setWeather(res.data)
+          console.log(res.data);
+        })
+  }, [])
+  
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -15,6 +26,11 @@ const CountryDetail = ({ country }) => {
       </ul>
 
       <img src={country.flags.png} alt={country.name.common} width={200} height={200} />
+
+      <h2>Wheather in {country.name.common}</h2>
+      <p>temperature {weather?.main.temp} Celcius</p>
+      <img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} />
+      <p>wind {weather?.wind.speed} m/s</p>
     </div>
   )
 }
