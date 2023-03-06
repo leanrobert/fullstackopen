28e1e3696cdf347293fe5ed7b7fcc0a9a10abddc
Leandro Robert <leanrobert24@gmail.com>
2023-03-06 10:31:00 -0300

    Exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -33,7 +33,18 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.send(data)
+  res.json(data)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = data.filter(phone => phone.id === id)
+
+  if(person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.listen(3001, () => {
