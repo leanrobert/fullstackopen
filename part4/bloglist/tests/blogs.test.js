f1aaf45d6b352990b99a914f2c55a56c770ff8e3
Leandro Robert <leanrobert24@gmail.com>
2023-03-07 14:01:25 -0300

    Exercise 4.5

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -29,3 +29,27 @@ describe('total likes', () => {
     expect(result).toBe(14)
   })
 })
+
+describe('most likes', () => {
+  test('empty list has no favourite', () => {
+    const blogs = []
+    const result = listHelper.favouriteBlog(blogs)
+    expect(result).toBe("List is empty")
+  })
+
+  test('when list has only one blog equals to the likes of that', () => {
+    const blogs = [{ title: "1", author: "1", url: "1", likes: 10 }]
+    const result = listHelper.favouriteBlog(blogs)
+    expect(result).toEqual({ title: "1", author: "1", likes: 10 })
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      { title: "1", author: "1", url: "1", likes: 10 },
+      { title: "2", author: "2", url: "2", likes: 11 },
+      { title: "3", author: "3", url: "3", likes: 3 },
+    ]
+    const result = listHelper.favouriteBlog(blogs)
+    expect(result).toEqual({ title: "2", author: "2", likes: 11 })
+  })
+ })
--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -6,7 +6,21 @@ const totalLikes = blogs => {
   return blogs.reduce((acc, blog) => acc + blog.likes, 0)
 }
 
+const favouriteBlog = blogs => {
+  if(blogs.length === 0) return "List is empty"
+
+  const mostLikes = Math.max(...blogs.map(blog => blog.likes))
+  const blogSelected = blogs.find(blog => blog.likes === mostLikes)
+
+  return {
+    title: blogSelected.title,
+    author: blogSelected.author,
+    likes: blogSelected.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
