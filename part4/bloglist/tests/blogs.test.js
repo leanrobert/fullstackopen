72dc10f88c8bc020e971e01a5d51cb45b177dc33
Leandro Robert <leanrobert24@gmail.com>
2023-03-07 13:44:05 -0300

    Exercise 4.4

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -5,3 +5,27 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals to the likes of that', () => {
+    const blogs = [{ title: "1", author: "1", url: "1", likes: 10 }]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(10)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      { title: "1", author: "1", url: "1", likes: 10 },
+      { title: "2", author: "2", url: "2", likes: 1 },
+      { title: "3", author: "3", url: "3", likes: 3 },
+    ]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(14)
+  })
+})
