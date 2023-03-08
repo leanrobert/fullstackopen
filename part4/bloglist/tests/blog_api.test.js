e814df142e7d268d1168b1150a699b0d3e0b5ce1
Leandro Robert <leanrobert24@gmail.com>
2023-03-08 11:10:15 -0300

    Exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -8,6 +8,11 @@ test('should return blog list as JSON', async () => {
   await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
 }, 100000)
 
+test('identifier field should be named id, not _id', async () => {
+  const blogs = await api.get('/api/blogs')
+  expect(blogs.body[0].id).toBeDefined()
+}, 100000)
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
