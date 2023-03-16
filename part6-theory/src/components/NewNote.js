72109c149fef8ef908b1c7e627e472b382280d24
Leandro Robert <leanrobert24@gmail.com>
2023-03-16 11:50:15 -0300

    Updates to the theory

--- part6-theory/src/components/NewNote.js
+++ part6-theory/src/components/NewNote.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
-import noteService from '../services/notes'
 
 const NewNote = () => {
   const dispatch = useDispatch()
@@ -10,8 +9,7 @@ const NewNote = () => {
     e.preventDefault()
     const content = e.target.note.value
     e.target.note.value = ''
-    const newNote = await noteService.createNew(content)
-    dispatch(createNote(newNote))
+    dispatch(createNote(content))
   }
 
   return (
