dfafd050373cdb11050f42532b223b1d01beacf5
Leandro Robert <leanrobert24@gmail.com>
2023-03-16 15:02:33 -0300

    updates on theory

--- part6-theory/src/App.js
+++ part6-theory/src/App.js
@@ -1,22 +1,54 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import NewNote from './components/NewNote'
-import Notes from './components/Notes'
-import VisibilityFilter from './components/VisibilityFilter'
-import { initializeNotes } from './reducers/noteReducer'
+import { useMutation, useQuery, useQueryClient } from "react-query"
+import { createNote, getNotes, updateNote } from './requests'
 
 const App = () => {
-  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
 
-  useEffect(() => {
-    dispatch(initializeNotes())
-  }, [dispatch])
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData('notes')
+      queryClient.setQueryData('notes', notes.concat(newNote))
+    }
+  })
+
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    newNoteMutation.mutate({ content, important: true })
+  }
+
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('notes')
+    }
+  })
+
+  const toggleImportance = (note) => {
+    updateNoteMutation.mutate({ ...note, important: !note.important })
+  }
+
+  const result = useQuery('notes', getNotes, { refetchOnWindowFocus: false })
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
 
   return(
     <div>
-      <NewNote />
-      <VisibilityFilter />
-      <Notes />
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map(note =>
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong> {note.important ? 'important' : ''}</strong>
+        </li>
+      )}
     </div>
   )
 }
