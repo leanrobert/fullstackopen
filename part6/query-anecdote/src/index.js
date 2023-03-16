2f637578c6f130fca554b43ea5aea154ea930218
Leandro Robert <leanrobert24@gmail.com>
2023-03-16 15:29:41 -0300

    Exercise 6.20

---
+++ part6/query-anecdote/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import App from './App'
+
+const clientQuery = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={clientQuery}>
+    <App />
+  </QueryClientProvider>
+)
