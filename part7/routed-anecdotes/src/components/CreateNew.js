e339e9b8d229a3a5bbc262bb3665b1a8fe7e9ffd
Leandro Robert <leanrobert24@gmail.com>
2023-03-17 16:09:24 -0300

    Exercise 7.5

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -15,6 +15,12 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -31,7 +37,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' type={info.type} value={info.value} onChange={info.onChange} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
