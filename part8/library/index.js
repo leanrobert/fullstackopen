a57d8695b66f0798cf9a6a124b6b38cd8cf877e5
Leandro Robert <leanrobert24@gmail.com>
2023-03-23 14:38:59 -0300

    Exercise 8.7

--- part8/library/index.js
+++ part8/library/index.js
@@ -127,6 +127,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -172,6 +176,16 @@ const resolvers = {
       }
       books = books.concat(book)
       return book
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      console.log(authors);
+      if(!author) return null
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
