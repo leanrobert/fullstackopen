6041208aa6b0c9db65c7a5cb8ac1146275fb5109
Leandro Robert <leanrobert24@gmail.com>
2023-03-31 16:17:14 -0300

    Exercise 9.8

---
+++ part9/patientor/backend/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
