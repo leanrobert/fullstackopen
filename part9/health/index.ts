73710145042cb12de0620cdb3d92f11564a89a1e
Leandro Robert <leanrobert24@gmail.com>
2023-03-31 14:46:16 -0300

    Exercise 9.5

--- part9/health/index.ts
+++ part9/health/index.ts
@@ -1,8 +1,28 @@
 import express = require('express');
+import { calculateBmi } from './bmiCalculator';
 const app = express()
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+})
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query
+  try {
+    const bmi = calculateBmi(Number(height), Number(weight))
+    res.send({
+      weight,
+      height,
+      bmi
+    });
+
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message
+    }
+    res.json({error: errorMessage});
+  }
 })
 
 const PORT = 3003
