53386cae9fbc5c5a9c2cdb71e1440eede69cf315
Leandro Robert <leanrobert24@gmail.com>
2023-04-04 10:49:19 -0300

    Exercise 9.25

--- part9/patientor/frontend/src/components/PatientDetailPage/PatientEntriesDetails.tsx
+++ part9/patientor/frontend/src/components/PatientDetailPage/PatientEntriesDetails.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react'
 import { Diagnose, Entry } from '../../types'
 import { getDiagnoses } from '../../services/diagnoses';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import WorkIcon from '@mui/icons-material/Work';
 
 interface Props {
   entry?: Entry
@@ -14,16 +18,51 @@ const PatientEntriesDetails = ({ entry }: Props) => {
     getDiagnoses().then(res => setDiagnoses(res.filter((diag: Diagnose) => entry?.diagnosisCodes?.includes(diag.code))))
   }, [entry?.diagnosisCodes])
 
-  return (
-    <div>
-      <p>{entry?.date} {entry?.description}</p>
-      <ul>
-        {entry?.diagnosisCodes?.map((diagnose, i) =>
-          <li key={diagnose}>{diagnose} {diagnoses[i]?.name}</li>
-        )}
-      </ul>
-    </div>
-  )
+  switch(entry?.type) {
+    case "HealthCheck":
+      return (
+        <div style={{ border: '1px solid black', padding: '0 5px' }}>
+          <p>{entry?.date} <MedicalServicesIcon /> </p>
+          <p>{entry?.description}</p>
+          <FavoriteIcon style={entry.healthCheckRating === 0 ? { color: 'green' } : { color: 'yellow' } } />
+          <ul>
+            {entry?.diagnosisCodes?.map((diagnose, i) =>
+              <li key={diagnose}>{diagnose} {diagnoses[i]?.name}</li>
+            )}
+          </ul>
+          <p>diagnose by {entry.specialist}</p>
+        </div>
+      )
+    case "Hospital":
+      return (
+        <div style={{ border: '1px solid black', padding: '0 5px' }}>
+          <p>{entry?.date} <LocalHospitalIcon /> </p>
+          <p>{entry?.description}</p>
+          <FavoriteIcon style={entry.healthCheckRating === 0 ? { color: 'green' } : { color: 'yellow' } } />
+          <ul>
+            {entry?.diagnosisCodes?.map((diagnose, i) =>
+              <li key={diagnose}>{diagnose} {diagnoses[i]?.name}</li>
+            )}
+          </ul>
+          <p>diagnose by {entry.specialist}</p>
+        </div>
+      )
+    case "OccupationalHealthcare":
+      return (
+        <div style={{ border: '1px solid black', padding: '0 5px' }}>
+          <p>{entry?.date} <WorkIcon /> </p>
+          <p>{entry?.description}</p>
+          <ul>
+            {entry?.diagnosisCodes?.map((diagnose, i) =>
+              <li key={diagnose}>{diagnose} {diagnoses[i]?.name}</li>
+            )}
+          </ul>
+          <p>diagnose by {entry.specialist}</p>
+        </div>
+      )
+  }
+
+  return null
 }
 
 export default PatientEntriesDetails
